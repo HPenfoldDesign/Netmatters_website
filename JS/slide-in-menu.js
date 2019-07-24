8288a5d1a4fc658b85e01e59a742a1aab7ed11df
HPenfoldDesign <48204704+HPenfoldDesign@users.noreply.github.com>
2019-07-24 15:18:04 +0100

    sidebar working now inserting extra menu on breakpoint

--- JS/slide-in-menu.js
+++ JS/slide-in-menu.js
@@ -2,13 +2,24 @@ var toggleStatus = 1;
 function toggleMenu() {
   if (toggleStatus === 1) {
     //document.getElementsByClassName('sidebar').style.right = "0px";
-    // alert("test");
     $(".sidebar").css("right", "0");
+    $("header").addClass("fixed-header");
+    $(".sidebar").css("position", "fixed"); 
+    $(".overlay").css("display", "block");
 
     toggleStatus = 0;
   } else if (toggleStatus === 0) {
     // document.getElementById('sidebar').style.right = " -320px";
     $(".sidebar").css("right", "-320px");
+    $("header").removeClass("fixed-header");
+    $(".overlay").css("display", "none");
+  
     toggleStatus = 1;
   }
-}
+} 
+
+$(".overlay").on("click", function(){
+  if($(".sidebar").css("right") === "0px") {
+    toggleMenu();
+  }
+});
